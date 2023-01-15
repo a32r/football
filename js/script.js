ed5eb7e21f6a5d689b876bd76cf5393d8084c96b
Admin <site@mail.com>
2023-01-16 00:16:31 +0300

    Added jquery function for change background of count div

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	lCountArr = $('.lCount').text();
 	rCountArr = $('.rCount').text();
-	console.log(lCountArr[1]);
-	console.log(rCountArr[1]);
+	// console.log(lCountArr[1]);
+	// console.log(rCountArr[1]);
+	$('.row').eq(1).children().eq(1).css('background', 'magenta');
 });
