5a58ebdcb82daa1f02ab16976650a5c6de412eb9
Admin <site@mail.com>
2023-01-18 23:25:57 +0300

    Added guest games card

--- index.php
+++ index.php
@@ -12,14 +12,14 @@
 function debug($arr) {
 	echo "<pre>";
 	print_r($arr);
-	/* var_dump($arr); */
 	echo "</pre>";
 }
 /* define functions end */
 
 $teams = ["Ливерпуль", "Челси", "Тоттенхэм Хотспур", "Арсенал", "Манчестер Юнайтед", "Эвертон", "Лестер Сити", "Вест Хэм Юнайтед", "Уотфорд", "Борнмут", "Бернли", "Саутгемптон", "Брайтон энд Хоув Альбион", "Норвич Сити", "Шеффилд Юнайтед", "Фулхэм", "Сток Сити", "Мидлсбро", "Суонси Сити", "Дерби Каунти"];
 
-/* shuffle($teams); */
+$guest = $teams;
+
 echo <<<HEADER
 <div class="pane">
 	<h3>Игра дома</h3>
@@ -37,6 +37,7 @@ for ($i = 1; $i <= $games; $i++) {
 	$team2=array_pop($teams);
 	$count1=rand(0,7);
 	$count2=rand(0,7);
+
 	echo <<<ROW
 	<div class="row">
 		<div>$team1</div>
@@ -45,10 +46,36 @@ for ($i = 1; $i <= $games; $i++) {
 	</div>
 ROW;
 }
+echo "</div>";
 
+echo <<<HEADER
+<div class="pane">
+	<h3>Игра в гостях</h3>
+	<div class="header">
+		<div>Дома</div>
+		<div>Счёт</div>
+		<div>В гостях</div>
+	</div>
+HEADER;
+
+for ($i = 1; $i <= $games; $i++) {
+	shuffle($guest);
+	$team1=array_pop($guest);
+	$team2=array_pop($guest);
+	$count1=rand(0,7);
+	$count2=rand(0,7);
+
+	echo <<<ROW
+	<div class="row">
+		<div>$team1</div>
+		<div><span class="lCount">$count1</span> &nbsp:&nbsp <span class="rCount">$count2</span></div>
+		<div>$team2</div>
+	</div>
+ROW;
+}
+echo "</div>";
 ?>
 
-</div>
 <script src="js/app.min.js"></script>
 </body>
 </html>
