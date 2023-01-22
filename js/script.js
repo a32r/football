e1b294c8412d9dfcc19d48b63187aff6dc090471
Admin <site@mail.com>
2023-01-22 14:23:11 +0300

    Added gradient to count div to all panes on the page

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
 $(document).ready(function() {
-	pane = $('.pane').eq(0).children('.row');
-	for (i = 1; i <= pane.length; i++) {
-		lCount = pane.find('.lCount').text()[i];
-		rCount = pane.find('.rCount').text()[i];
-		if (lCount < rCount) {
-			pane.eq(i).find('.rCount').css({
-				'font-weight': 'bold',
-				'font-size': 'xx-large'
-			});
-			countDiv = pane.eq(i).children().eq(1);
-			countDiv.css('background', 'linear-gradient(to left, lightgreen, #FF4D4D)');
-		}
-		if (lCount > rCount) {
-			pane.eq(i).find('.lCount').css({
-				'font-weight': 'bold',
-				'font-size': 'xx-large'
-			});
-			countDiv = pane.eq(i).children().eq(1);
-			countDiv.css('background', 'linear-gradient(to right, lightgreen, #FF4D4D)');
+	panes = $('.pane');
+	for (p = 0; p < panes.length; p++) {
+		pane = $('.pane').eq(p);
+		rows = pane.children('.row');
+		for (i = 1; i <= rows.length; i++) {
+			lCount = rows.find('.lCount').text()[i];
+			rCount = rows.find('.rCount').text()[i];
+			if (lCount < rCount) {
+				rows.eq(i).find('.rCount').css({
+					'font-weight': 'bold',
+					'font-size': 'xx-large'
+				});
+				countDiv = rows.eq(i).children().eq(1);
+				countDiv.css('background', 'linear-gradient(to left, lightgreen, #FF4D4D)');
+			}
+			if (lCount > rCount) {
+				rows.eq(i).find('.lCount').css({
+					'font-weight': 'bold',
+					'font-size': 'xx-large'
+				});
+				countDiv = rows.eq(i).children().eq(1);
+				countDiv.css('background', 'linear-gradient(to right, lightgreen, #FF4D4D)');
+			}
 		}
 	}
 });
